db7be1794fb2928c38db88b4e2e17d12c924ba76
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-12-27 22:53:06 -0500

    script for resources usage

--- digitalscript.sh
+++ digitalscript.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# colors
-Red=$'\e[1;31m'
-Green=$'\e[1;32m'
-Blue=$'\e[1;314m'
+# # colors
+# Red=$'\e[1;31m'
+# Green=$'\e[1;32m'
+# Blue=$'\e[1;314m'
 
-# show time on terminal while script runs
-while true; do
-    clear
-    echo $Green $(date +%T)
-    sleep 1s
-done
+# # show time on terminal while script runs
+# while true; do
+#     clear
+#     echo $Green $(date +%T)
+#     sleep 1s
+# done
+
+
+cpu_usage=$(top -b -n 1 | grep "%Cpu(s)" | awk '{print $2}')
+memory_usage=$(free -m | awk '/Mem/{print $3}')
+disk_usage=$(df -h / | awk 'NR==2 {print $5}')
+
+echo "CPU Usage: $cpu_usage%"
+echo "Memory Usage: $memory_usage MB"
+echo "Disk Usage: $disk_usage"
