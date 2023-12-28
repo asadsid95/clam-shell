382c7c47e6d2c246ade8d0934ad7e324670860ca
Asad Siddiqui <asadsid95@gmail.com>
2023-12-28 18:56:42 -0500

    basic crontab setup to run script every minute

--- main.sh
+++ main.sh
@@ -1,6 +1,12 @@
-echo "File name: $0"
+echo "File name: $0" 
 echo "1st parameter: $1"
 echo "2nd parameter: $2"
 echo "Each arg kept as individual entity: $@"
 echo "All args as one entity: $*"
 echo "Total arg supplied: $#"
+
+# log into result.txt every minute
+echo 'hello' >> result.txt
+
+# crontab -e -- to check for existing crontab
+# * * * * * sh /Users/asad/Desktop/clam-shell/main.sh
