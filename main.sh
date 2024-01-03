5af62353631b71bbf999beabf5eaa70aebc08828
Asad Siddiqui <asadsid95@gmail.com>
2024-01-02 22:28:32 -0500

    script to print current working directory

--- main.sh
+++ main.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-current_dir=$1
-target_dir=$2
+current_dir=$(pwd)
 
-if [ $# -ne 2 ]; then
-    echo "need 2 arguments"
-    exit 1
-fi
-
-
-tar -cvzf "backup_archive_file" "/Users/asad/Desktop/clam-shell"
+echo $current_dir
