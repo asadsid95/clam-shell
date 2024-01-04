b81b51424f63ad02ce79b8f0caee8e6fd7ff5fbb
Asad Siddiqui <asadsid95@gmail.com>
2024-01-03 22:29:04 -0500

    script for renaming files in a directory

---
+++ new_main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+directory=$(pwd)
+
+prefix="new_"
+
+cd "$directory" || exit
+
+# looping through each file in directory
+for file  in *; do
+    if [ -f "$file" ]; then
+        new_filename="$prefix$file"
+
+        # rename the file
+        mv "$file" "$new_filename"
+
+        echo "Renamed: $file to $new_filename"
+
+    fi
+done
+
+echo "Batch renaming completed"
