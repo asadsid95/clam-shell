f8478b43f3785202220eda97ba58629d072da5ab
Asad Siddiqui <asadsid95@gmail.com>
2024-01-06 13:33:04 -0500

    script with array expansion for iterating

--- new_main.sh
+++ new_main.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-directory=$(pwd)
+ip_address=("000.000.000.000" "000.000.000.001" "000.000.000.002")
 
-prefix="new_"
+for ip in "${ip_address[@]}"; do
+    echo $ip
 
-cd "$directory" || exit
-
-# looping through each file in directory
-for file  in *; do
-    if [ -f "$file" ]; then
-        new_filename="$prefix$file"
-
-        # rename the file
-        mv "$file" "$new_filename"
-
-        echo "Renamed: $file to $new_filename"
-
-    fi
-    
 done
-
-echo "Batch renaming completed"
