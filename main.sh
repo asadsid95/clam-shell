989ae8e625956f2af2b1dd7e3f4093ec41d2350d
Asad Siddiqui <asadsid95@gmail.com>
2024-01-08 23:05:52 -0500

    script with function

--- main.sh
+++ main.sh
@@ -2,8 +2,14 @@
 
 urls=("google.com" "news.ycombinator.com")
 
-for url in "${urls[@]}"; do
-    result=$(ping -c 1 $url 2>&1)
-
+check_server() {
+    local result=$(ping -c 1 $url 2>&1)
+    
     echo $result "/n"
+
+}
+
+
+for url in "${urls[@]}"; do
+    check_server "$url"
 done
