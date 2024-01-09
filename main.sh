92b9253165a154f489de89ae9d9e7d64fa70c001
Asad Siddiqui <asadsid95@gmail.com>
2024-01-08 23:18:24 -0500

    script with functions

--- main.sh
+++ main.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-urls=("google.com" "news.ycombinator.com")
+urls=("google.comm" "news.ycombinator.com")
 
 check_server() {
-    local result=$(ping -c 1 $url 2>&1)
+
+    local server=$1
+    local result=$(ping -c 1 "$server" 2>&1)
     
+    if [[ $? -eq 0 ]]; then
+        echo "$server is reachable===========; /n Response: $result"
+    else
+        echo "$server is unreachable;^^^^^^^^^^^ Error: $result"
+    fi
+
     echo $result "/n"
 
 }
 
 
 for url in "${urls[@]}"; do
+    # check_server 
     check_server "$url"
 done
