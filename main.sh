d48ab01c5a1140254a618cc2fcaba549039f35cd
Asad Siddiqui <asadsid95@gmail.com>
2024-01-12 00:06:13 -0500

    script for reading number of fields and records

--- main.sh
+++ main.sh
@@ -2,10 +2,14 @@
 
 file_to_read="main.csv"
 
-result=$(awk -F ','  '{print $0}'  $file_to_read)
+result=`awk -F ','  '{print $0}'  $file_to_read`
 
 # echo -e "$result"
 
-awk 'NF>=2' $file_to_read
+awk  'NF>=0'  $file_to_read
+
 
 # tail -n +5 $file_to_read
+
+files=`ls -l | wc -l`
+echo "Files in $(pwd) directory: $files"
