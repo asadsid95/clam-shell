44baccf349b7f00160beb5dc65abde9cac179bcf
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2024-01-25 23:49:56 -0500

    script to modify csv column's values

--- main.sh
+++ main.sh
@@ -2,4 +2,7 @@
 
 # cat project.csv
 
-awk -v "project.csv"
+file="project.csv"
+file_mod="project1.csv"
+
+awk -v to_add="<API_TOKEN>" 'BEGIN {FS=OFS=","} {if (NR > 1) $2 = to_add} {print}' "$file" > "$file_mod"
