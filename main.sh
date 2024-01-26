b99c0c8cec0e917d8dadae493149ada68104c5bb
root <root@Asad.localdomain>
2024-01-25 23:39:47 -0500

    script to modify values in a column

--- main.sh
+++ main.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-echo "Username: "
-read username
+# cat project.csv
 
-generate_password() {
-    openssl rand -base64 12 | tr -d "+/="
-}
-
-password=$(generate_password)
-
-send_email(){
-    local to_address=$1
-    local suject=$2
-    local body=$3
-
-    echo "$body" | mail -s "$subject" $to_address
-
-}
-
-
-
-send_email "asads@hey.com" "test from script" "hello world"
+awk -v "project.csv"
